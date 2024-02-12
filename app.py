fba5c6f5d8edd265ccea81fb3193d98688d2d768
Rahul Rawat <rahulrawatr320@gmail.com>
2024-02-12 16:01:42 +0530

    website to practice authentication

---
+++ app.py
@@ -0,0 +1,119 @@
+from flask import Flask, render_template, request, jsonify,redirect,url_for
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+# from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+db_string=os.environ.get("DB_STRING")
+secret_key=os.environ.get("SECRET_KEY")
+
+app=Flask(__name__)
+app.config['SECRET_KEY'] = secret_key
+# app.config['SQLALCHEMY_DATABASE_URI'] = "ente db_url"
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+engine=create_engine(db_string)
+
+# db=SQLAlchemy(app)
+login_manager=LoginManager(app)
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.get(user_id)
+
+class User(UserMixin):
+  def __init__(self,user_id,name,email):
+    self.id=user_id
+    self.name=name
+    self.email=email
+
+  @staticmethod
+  def get(user_id):
+    with engine.connect() as conn:
+      query=text("SELECT name,email FROM users where id=:user_id")
+      parameters={"user_id":user_id}
+      result=conn.execute(query,parameters)
+      conn.commit()
+      result=result.all()
+      result=result[0]._asdict()
+      return User(user_id,result["name"],result["email"])
+
+
+@app.route("/")
+def home():
+  return render_template("home.html")
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+  if request.method=="POST":
+    data=request.form
+    email,password=data["email"],data["password"]
+    with engine.connect() as conn:
+      query=text("SELECT id,name,email,password FROM users WHERE email=:email")
+      parameters={
+        "email":email
+      }
+      result=conn.execute(query,parameters)
+      conn.commit()
+      entry=result.first()
+      if entry:
+        result=entry._asdict()
+      else:
+        return "Invalid Username"
+    if check_password_hash(result["password"],password):
+      user=User(user_id=result["id"],name=result["name"],email=result["email"])
+      login_user(user)
+      return redirect(url_for("dashboard"))
+    else:
+      return "Invalid Password"
+  return render_template("login.html")
+
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+  if request.method=="POST":
+    data=request.form
+    name,email,password=data["name"],data["email"],data["password"]
+    hashed_password=generate_password_hash(password)
+    with engine.connect() as conn:
+      # checking that user already exist or not
+      query=text("SELECT id,name,email,password FROM users WHERE email=:email")
+      parameters={
+        "email":email
+      }
+      result=conn.execute(query,parameters)
+      entry=result.first()
+      if entry:
+        conn.commit()
+        return "Email already found"
+      # writing signup to database
+      query=text("INSERT INTO users(name,email,password) VALUES(:name,:email,:password)")
+      parameters={
+        "name":name,
+        "email":email,
+        "password":hashed_password
+      }
+      conn.execute(query,parameters)
+      conn.commit()
+    return redirect(url_for("login"))
+  return render_template("register.html")
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+  return render_template("dashboard.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+  logout_user()
+  return redirect(url_for("login"))
+
+
+
+if __name__=="__main__":
+  app.run(host="0.0.0.0",debug=True)
